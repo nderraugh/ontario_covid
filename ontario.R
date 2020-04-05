add989e3e9c48956b2686ded80ac2b6f193e98dc
Neil Derraugh <nderraugh@gmail.com>
2020-04-05 13:34:47 -0400

    Update to cluster cases when zoomed out.

--- ontario.R
+++ ontario.R
@@ -7,9 +7,7 @@ lat <- -84.741725
 # https://data.ontario.ca/en/dataset/confirmed-positive-cases-of-covid-19-in-ontario
 on_geojson <- rgdal::readOGR("json/conposcovidloc.geojson")
 
-d <- on_geojson@data %>% 
-  group_by(Reporting_PHU_City, Reporting_PHU_Latitude, Reporting_PHU_Longitude) %>% 
-  summarise(case_count = n())
+d <- on_geojson@data 
 
 leaflet(on_geojson) %>% 
   addTiles() %>% 
@@ -17,8 +15,9 @@ leaflet(on_geojson) %>%
   addCircleMarkers(
     d$Reporting_PHU_Longitude, 
     d$Reporting_PHU_Latitude, 
-    d$case_count/mean(d$case_count), 
     popup = d$Reporting_PHU_City,
     fill=TRUE,
-    fillOpacity = 0.5
+    fillOpacity = 0.5,
+    stroke = 0,
+    clusterOptions = markerClusterOptions(spiderfyOnMaxZoom=F)
     )
